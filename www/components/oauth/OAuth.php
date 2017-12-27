252d145ffb08da1a19a07d08db772a8f91dcc42e
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-12-27 07:15:07 +0300

    Update

--- www/components/oauth/OAuth.php
+++ www/components/oauth/OAuth.php
@@ -8,6 +8,14 @@ use \Psr\Http\Message\ResponseInterface as Response;
 class OAuth
 {
 
+    /**
+     * Provide Google OAuth authorization flow
+     *
+     * @param Request $req
+     * @param Response $res
+     * @param $args
+     * @return Response
+     */
     public function code(Request $req, Response $res, $args)
     {
 
@@ -17,35 +25,41 @@ class OAuth
             'code' => $params['code'],
             'client_id' => $_SERVER['GOOGLE_CLIENT_ID'],
             'client_secret' => $_SERVER['GOOGLE_CLIENT_SECRET'],
-            'redirect_uri' => 'http://localhost:8081/oauth/code',
+            'redirect_uri' => $params['state'],
             'grant_type' => 'authorization_code'
         ];
 
         $url = 'https://www.googleapis.com/oauth2/v4/token';
+        $result = \App\System\HTTP::Request('POST', $url, $data);
 
-        $curl = curl_init();
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $result = curl_exec($curl);
-        curl_close($curl);
-
-        $token = @json_decode($result, true);
-        $curl = curl_init();
-
-
+        $token = @json_decode($result);
         $url =  'https://www.googleapis.com/userinfo/v2/me';
-        $header = 'Authorization: ' . $token['token_type'] . ' ' . $token['access_token'];
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, [$header]);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $profileinfo = curl_exec($curl);
-        curl_close($curl);
+        $header = 'Authorization: ' . $token->token_type . ' ' . $token->access_token;
 
+        $profileInfo = \App\System\HTTP::Request('GET', $url, [], [$header]);
+        $profileInfo = @json_decode($profileInfo);
 
-        echo '<div id="info">' . $profileinfo . '</div>';
+        $userData = [
+            'id' => 'g' . $profileInfo->id,
+            'photo' => $profileInfo->picture,
+            'name' => $profileInfo->name
+        ];
+
+        $user = new \App\Components\Api\Models\User($userData['id']);
+
+        $user->sync($userData);
+
+        $jwt = \Firebase\JWT\JWT::encode([
+            'iss' => 'CodeX Notes API Server',
+            'aud' => 'CodeX Notes Application',
+            'iat' => time(),
+            'id' => $userData['id'],
+            'photo' => $userData['photo'],
+            'name' => $userData['name'],
+        ], $userData['id'] . $_SERVER['USER_SALT']);
+
+        $body = $res->getBody();
+        $body->write('<div id="jwt">' . $jwt . '</div>');
 
         return $res;
 
--- www/system/Http.php
+++ www/system/Http.php
@@ -19,4 +19,41 @@ class HTTP
 
     CONST STRING_SERVER_ERROR = 'Internal server error';
     CONST STRING_NOT_ALLOWED_METHOD = 'Method must be one of: %s';
+
+    static public function Request($method, $url, $params=[], $headers=[])
+    {
+
+        $curl = curl_init();
+        switch (strtoupper($method)) {
+            case 'POST':
+                curl_setopt($curl, CURLOPT_POST,1);
+                if (count($params)) {
+                    curl_setopt($curl,CURLOPT_POSTFIELDS, http_build_query($params));
+                }
+                break;
+            case 'GET':
+                if (!count($params)) {
+                    break;
+                }
+
+
+                $url .= '?' . http_build_query($params);
+                break;
+            default:
+                throw new \Exception('Unsupported method');
+        }
+
+        if (count($headers)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
+
+
+    }
+
 }

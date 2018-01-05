1741b023e6d842cbb3930ced5920bb01a1b82ba6
N0str <ntpcp@yandex.ru>
2018-01-05 23:21:15 +0300

    Add autoload.php

---
+++ www/public/autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function ($classname) {
+    $pathParts = explode('\\', $classname);
+    $pathToFile = PROJECTROOT . '/';
+    for ($i = 1; $i < count($pathParts); $i++) {
+        $pathToFile .= ($i + 1 == count($pathParts)) ? $pathParts[$i] . '.php' : strtolower($pathParts[$i]) . '/';
+    }
+    if (file_exists($pathToFile)) {
+        require_once $pathToFile;
+    }
+});

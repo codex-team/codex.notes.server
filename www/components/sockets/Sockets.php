48f4e4c8555ef0163514516a0c58d85f0cd3b485
N0str <ntpcp@yandex.ru>
2018-02-03 23:09:29 +0300

    Add messages history support

--- www/components/sockets/Sockets.php
+++ www/components/sockets/Sockets.php
@@ -8,7 +8,7 @@ use App\System\Config;
 class Sockets
 {
     public static function push($channel, $message) {
-        $ch = curl_init(Config::get('SERVER_URI') . 'chan/'. $channel);
+        $ch = curl_init(Config::get('SERVER_URI') . 'chan/'. $channel . '.b10');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($message));
